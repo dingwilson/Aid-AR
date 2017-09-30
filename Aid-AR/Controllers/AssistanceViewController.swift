14a0038ecb32b325350beefa484c3e084dba5658
Wilson Ding <hello@wilsonding.com>
2017-09-30 16:44:25 -0500

    Added AssistanceViewController

---
+++ Aid-AR/Controllers/AssistanceViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AssistanceViewController.swift
+//  Aid-AR
+//
+//  Created by Wilson Ding on 9/30/17.
+//  Copyright © 2017 Wilson Ding. All rights reserved.
+//
+
+import UIKit
+
+class AssistanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func dismissVC(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
