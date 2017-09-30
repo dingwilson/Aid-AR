3364d4f6c512a336dc73cb3b4361de29e12bc9fc
Wilson Ding <hello@wilsonding.com>
2017-09-30 14:29:39 -0500

    Added basic AR functionality

---
+++ Aid-AR/Controllers/ARViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ARViewController.swift
+//  Aid-AR
+//
+//  Created by Wilson Ding on 9/30/17.
+//  Copyright © 2017 Wilson Ding. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARCL
+import CoreLocation
+
+class ARViewController: UIViewController, SceneLocationViewDelegate {
+    
+    @IBOutlet weak var arView: UIView!
+    
+    var sceneLocationView = SceneLocationView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        sceneLocationView.run()
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 32.7333446, longitude: -97.1105474)
+        let location = CLLocation(coordinate: coordinate, altitude: 300)
+        let image = UIImage(named: "pin")!
+        
+        let annotationNode = LocationAnnotationNode(location: location, image: image)
+    
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
+        
+        arView.addSubview(sceneLocationView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        sceneLocationView.frame = arView.bounds
+    }
+    
+    // MARK: - SceneLocationViewDelegat
+    
+    func sceneLocationViewDidAddSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
+        
+    }
+    
+    func sceneLocationViewDidRemoveSceneLocationEstimate(sceneLocationView: SceneLocationView, position: SCNVector3, location: CLLocation) {
+        
+    }
+    
+    func sceneLocationViewDidConfirmLocationOfNode(sceneLocationView: SceneLocationView, node: LocationNode) {
+        
+    }
+    
+    func sceneLocationViewDidSetupSceneNode(sceneLocationView: SceneLocationView, sceneNode: SCNNode) {
+        
+    }
+    
+    func sceneLocationViewDidUpdateLocationAndScaleOfLocationNode(sceneLocationView: SceneLocationView, locationNode: LocationNode) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
