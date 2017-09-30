9862a3db9ea5706b3612d8dd8b634199f422dcf2
Wilson Ding <hello@wilsonding.com>
2017-09-30 15:15:00 -0500

    Added basic splash page and setup screens

---
+++ Aid-AR/Controllers/SplashViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SplashViewController.swift
+//  Aid-AR
+//
+//  Created by Wilson Ding on 9/30/17.
+//  Copyright © 2017 Wilson Ding. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    @IBOutlet weak var backgroundVideo: BackgroundVideo!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        backgroundVideo.createBackgroundVideo(name: "Background", type: "mp4", alpha: 0.2)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
